9bec705bd78bc1fe07dd9c4daec5abc13305bded
peng <e359716712@qq.com>
2017-08-17 14:19:03 +0800

    html

---
+++ htm/css/link_chinese.js
@@ -0,0 +1,77 @@
+
+
+//2014.청담퍼스트성형외과
+
+
+
+
+//sub01. 
+function go_01_01() {
+	document.location.href="/cheongdam/cheongdam0101.html"; //인사말
+}
+
+function go_02_01() {
+	document.location.href="/cheongdam/cheongdam0201.html";//재수술
+}
+
+function go_03_01() {
+	document.location.href="/cheongdam/cheongdam0301.html";//페이스
+}
+
+function go_04_01() {
+	document.location.href="/cheongdam/cheongdam0401.html";//찾아오시는길
+}
+
+function go_05_01() {
+	document.location.href="/cheongdam/cheongdam0401.html";//진료시간
+}
+
+
+function go_06_01() {
+	document.location.href="/counsel_new/counsel_01_main.htm"; //상담
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//중문사이트. 
+function go_11_01() {
+	document.location.href="/cheongdam_chinese/cheongdam0101.html"; //인사말
+}
+
+function go_12_01() {
+	document.location.href="/cheongdam_chinese/cheongdam0201.html";//재수술
+}
+
+function go_13_01() {
+	document.location.href="/cheongdam_chinese/cheongdam0301.html";//페이스
+}
+
+function go_14_01() {
+	document.location.href="/cheongdam_chinese/cheongdam0401.html";//찾아오시는길
+}
+
+function go_15_01() {
+	document.location.href="/cheongdam_chinese/cheongdam0401.html";//진료시간
+}
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+//기타
+function go_index() {
+	document.location.href="/index.html";
+}
+
+function go_admin() {
+	document.location.href="/admin";
+}
+//////////////////////////////////////////////////////////////////////////////////
